8f7d62b022595704c4175fb2daba4168d3155a3f
damtrix <oludamola.onarinde@gamil.com>
2023-03-20 12:40:07 +0100

    adjust

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -6,17 +6,17 @@ import os
 
 
 class test_state(test_basemodel):
-    """ """
+    """ states test class"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ state test class init"""
         super().__init__(*args, **kwargs)
         self.name = "State"
         self.value = State
 
     def test_name3(self):
-        """ """
+        """ testing state name attr"""
         new = self.value()
-        self.assertEqual(type(new.name), str if 
-                        os.getenv('HBNB_TYPE_STORAGE') != 'db' else
-                        type(None))
+        self.assertEqual(type(new.name), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
