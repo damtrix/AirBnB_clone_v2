2563ef926b9bd136d97686cd2645a87ca226cb71
damtrix <oludamola.onarinde@gmail.com>
2023-04-06 01:50:44 -0700

    100-clean_web_static.py

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -2,7 +2,7 @@
 import os
 from fabric.api import *
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
+env.hosts = ["54.237.31.149", "34.224.1.2"]
 
 
 def do_clean(number=0):
@@ -20,10 +20,10 @@ def do_clean(number=0):
     archives = sorted(os.listdir("versions"))
     [archives.pop() for i in range(number)]
     with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
+        [local("sudo rm ./{}".format(a)) for a in archives]
 
     with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
+        archives = run("sudo ls -tr").split()
         archives = [a for a in archives if "web_static_" in a]
         [archives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in archives]
+        [run("sudo rm -rf ./{}".format(a)) for a in archives]
