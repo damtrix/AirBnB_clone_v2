47e4ed3163ec4abad7d087c99b8c93f3da3ae9d0
damtrix <oludamola.onarinde@gmail.com>
2023-04-21 07:51:09 +0100

    web_flask/1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -7,10 +7,12 @@ from flask import Flask
 
 app = Flask("__name__")
 
+
 @app.route('/', strict_slashes=False)
 def hello():
     """Return a given string"""
     return ("Hello HBNB!")
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
