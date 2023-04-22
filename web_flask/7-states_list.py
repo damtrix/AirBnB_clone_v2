63c20ce6bc921dc972cd40b7d71ce91c48d830d2
damtrix <oludamola.onarinde@gmail.com>
2023-04-22 20:03:25 +0100

    7-state

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -18,5 +18,11 @@ def states_list():
     return render_template("7-states_list.html", states=states)
 
 
+@app.teardown_appcontext
+def teardown(self):
+    """Method to remove current SQLAlchemy Session"""
+    storage.close()
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=None)
