af41eb0f00e874d5a0e5c4025c38fccf70217dcc
damtrix <oludamola.onarinde@gmail.com>
2023-04-22 18:33:23 +0100

    update

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -39,7 +39,7 @@ def number(n):
     return "{} is a number".format(n)
 
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     """Displays an HTML page only if <n> is an integer."""
     return render_template("5-number.html", n=n)
